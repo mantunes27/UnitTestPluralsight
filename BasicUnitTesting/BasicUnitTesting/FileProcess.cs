9b82455da1d0d25240e2029a0f92b81eeb64a6e9
Marco Antunes <marcoantunes86@gmail.com>
2017-07-19 16:08:31 +0100

    add

---
+++ BasicUnitTesting/BasicUnitTesting/FileProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BasicUnitTesting
+{
+    public class FileProcess
+    {
+        public bool FileExists(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentNullException("fileName");
+            }
+
+            return File.Exists(fileName);   
+         }
+    }
+}
---
+++ BasicUnitTesting/BasicUnitTestingTest/FileProcessTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BasicUnitTesting;
+using System.Configuration;
+using System.IO;
+
+namespace BasicUnitTestingTest
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        private const string BAD_FILE_NAME = @"C:\Windows\Regedierererererereret.exe";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void FileNameDoesExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            SetGoodFileName();
+            TestContext.WriteLine("Creating the file: " + _GoodFileName);
+            File.AppendAllText(_GoodFileName, "Some text");
+            TestContext.WriteLine("Testing the file: " + _GoodFileName);
+            fromCall = fp.FileExists(_GoodFileName);
+            TestContext.WriteLine("Deleting the file: " + _GoodFileName);
+            File.Delete(_GoodFileName);
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExists()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+
+            fp.FileExists("");
+        }
+
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullExceptionTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                //the test was a success
+                return;
+            }
+
+            Assert.Fail("Call to FileExists did not throw an Argument Null Exception");
+        }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+    }
+}
