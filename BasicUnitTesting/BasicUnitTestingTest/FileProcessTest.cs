1fbb9826713ddfaab183257fcf0c770cf61a2f57
mantunes27 <marcoantunes86@gmail.com>
2017-07-23 00:30:51 +0100

    added

--- BasicUnitTesting/BasicUnitTestingTest/FileProcessTest.cs
+++ BasicUnitTesting/BasicUnitTestingTest/FileProcessTest.cs
@@ -12,6 +12,48 @@ namespace BasicUnitTestingTest
         private const string BAD_FILE_NAME = @"C:\Windows\Regedierererererereret.exe";
         private string _GoodFileName;
 
+        #region Class Initialize and Cleanup
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext tc)
+        {
+            tc.WriteLine("In the Class Initialize.");
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+        #endregion
+
+       
+        [TestInitialize]
+        public void TestInitialize()
+        {
+           if (TestContext.TestName == "FileNameDoesExist") SetGoodFileName();
+            {
+                SetGoodFileName();
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine("Creating File: " + _GoodFileName);
+                    File.AppendAllText(_GoodFileName, "Some Text");
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (TestContext.TestName == "FileNameDoesExist")
+            {
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine("Deleting File: " + _GoodFileName);
+                    File.Delete(_GoodFileName);
+                }
+            }
+        }
+
+
         public TestContext TestContext { get; set; }
 
         [TestMethod]
@@ -19,15 +61,7 @@ namespace BasicUnitTestingTest
         {
             FileProcess fp = new FileProcess();
             bool fromCall;
-
-            SetGoodFileName();
-            TestContext.WriteLine("Creating the file: " + _GoodFileName);
-            File.AppendAllText(_GoodFileName, "Some text");
-            TestContext.WriteLine("Testing the file: " + _GoodFileName);
             fromCall = fp.FileExists(_GoodFileName);
-            TestContext.WriteLine("Deleting the file: " + _GoodFileName);
-            File.Delete(_GoodFileName);
-
             Assert.IsTrue(fromCall);
         }
 
@@ -47,7 +81,6 @@ namespace BasicUnitTestingTest
         public void FileNameNullOrEmpty_ThrowsArgumentNullException()
         {
             FileProcess fp = new FileProcess();
-
             fp.FileExists("");
         }
 
